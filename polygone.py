c814d53c11e0dd0ed631d2089a5c47bf735bd148
louteranas <louteranas@gmail.com>
2019-10-14 16:23:37 +0200

    debug

--- polygone.py
+++ polygone.py
@@ -1,22 +1,23 @@
+#!/usr/bin/env python3
+
 class Polygone:
     def __init__(self, points = []):
         self.points = points;
 
     def partialDecompose(n):
         for i in range(n//2):
-            self.points[i] = (0.25 * (-self.points[(2*i-2)%length(self.points)] +
-                                    3*self.points[(2*i-1)%length(self.points)] + 
-                                    3*self.points[(2*i)%length(self.points)] - 
+            self.points[i] = (0.25 * (-self.points[(2*i-2)%length(self.points)] +\
+                                    3*self.points[(2*i-1)%length(self.points)] + \
+                                    3*self.points[(2*i)%length(self.points)] - \
                                     self.points[(2*i+1)%length(self.points)]));
-            self.points[i + (n//2)] = (0.25 * (self.points[(2*i-2)%length(self.points)] -
-                                    3*self.points[(2*i-1)%length(self.points)] + 
-                                    3*self.points[(2*i)%length(self.points)] - 
+            self.points[i + (n//2)] = (0.25 * (self.points[(2*i-2)%length(self.points)] -\
+                                    3*self.points[(2*i-1)%length(self.points)] + \
+                                    3*self.points[(2*i)%length(self.points)] - \
                                     self.points[(2*i+1)%length(self.points)]));
     
     def partialRecompose(n):
         for i in range(n):
-            self.points[2*i] = 0.75 * (self.points[i] + self.points[n + i]) + 
+            self.points[2*i] = 0.75 * (self.points[i] + self.points[n + i]) + \
                                0.25 * (self.points[i + 1] - self.points[n + i + 1]);
-            self.points[2*i +1] = 0.25 * (self.points[i] + self.points[n + i]) +
+            self.points[2*i +1] = 0.25 * (self.points[i] + self.points[n + i]) +\
                                   0.75 * (self.points[i + 1] - self.points[n + i + 1]);
-                                  
