f17ffc9f5d871837a1daffcd9b96728e2d52136b
louteranas <louteranas@gmail.com>
2019-10-14 15:35:28 +0200

    start calss polygone

---
+++ polygone.py
@@ -0,0 +1,11 @@
+class Polygone:
+    def __init__(self, x = [], y = []):
+        self.x = x;
+        self.y = y;
+
+    def partialDecompose():
+        decomposedX = [];
+        decomposedY = [];
+        for i in range(length(self.x)/2):
+            
+            
