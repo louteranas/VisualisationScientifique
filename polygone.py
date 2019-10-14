a5ee92688b5554029fbc39b16ddd44695794ab7f
louteranas <louteranas@gmail.com>
2019-10-14 16:20:31 +0200

    partial decompose and recompose done

--- polygone.py
+++ polygone.py
@@ -1,11 +1,22 @@
 class Polygone:
-    def __init__(self, x = [], y = []):
-        self.x = x;
-        self.y = y;
+    def __init__(self, points = []):
+        self.points = points;
 
-    def partialDecompose():
-        decomposedX = [];
-        decomposedY = [];
-        for i in range(length(self.x)/2):
-            
-            
+    def partialDecompose(n):
+        for i in range(n//2):
+            self.points[i] = (0.25 * (-self.points[(2*i-2)%length(self.points)] +
+                                    3*self.points[(2*i-1)%length(self.points)] + 
+                                    3*self.points[(2*i)%length(self.points)] - 
+                                    self.points[(2*i+1)%length(self.points)]));
+            self.points[i + (n//2)] = (0.25 * (self.points[(2*i-2)%length(self.points)] -
+                                    3*self.points[(2*i-1)%length(self.points)] + 
+                                    3*self.points[(2*i)%length(self.points)] - 
+                                    self.points[(2*i+1)%length(self.points)]));
+    
+    def partialRecompose(n):
+        for i in range(n):
+            self.points[2*i] = 0.75 * (self.points[i] + self.points[n + i]) + 
+                               0.25 * (self.points[i + 1] - self.points[n + i + 1]);
+            self.points[2*i +1] = 0.25 * (self.points[i] + self.points[n + i]) +
+                                  0.75 * (self.points[i + 1] - self.points[n + i + 1]);
+                                  
