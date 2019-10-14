ccac2f9904d53741c727635fb236a5d2ec53156e
david540 <david.desobry@orange.fr>
2019-10-14 16:30:27 +0200

    Update polygone.py

--- polygone.py
+++ polygone.py
@@ -9,15 +9,26 @@ class Polygone:
             self.points[i] = (0.25 * (-self.points[(2*i-2)%length(self.points)] +\
                                     3*self.points[(2*i-1)%length(self.points)] + \
                                     3*self.points[(2*i)%length(self.points)] - \
-                                    self.points[(2*i+1)%length(self.points)]));
+                                    self.points[(2*i+1)%length(self.points)]))
             self.points[i + (n//2)] = (0.25 * (self.points[(2*i-2)%length(self.points)] -\
                                     3*self.points[(2*i-1)%length(self.points)] + \
                                     3*self.points[(2*i)%length(self.points)] - \
-                                    self.points[(2*i+1)%length(self.points)]));
-    
+                                    self.points[(2*i+1)%length(self.points)]))
+
     def partialRecompose(n):
         for i in range(n):
             self.points[2*i] = 0.75 * (self.points[i] + self.points[n + i]) + \
-                               0.25 * (self.points[i + 1] - self.points[n + i + 1]);
+                               0.25 * (self.points[i + 1] - self.points[n + i + 1])
             self.points[2*i +1] = 0.25 * (self.points[i] + self.points[n + i]) +\
-                                  0.75 * (self.points[i + 1] - self.points[n + i + 1]);
+                                  0.75 * (self.points[i + 1] - self.points[n + i + 1])
+    def decompositionTotale():
+        n = len(self.points)
+        while n > 4:
+            self.partialDecompose(n)
+            n /= 2
+
+    def recompositionTotale():
+        n = 4
+        while n < len(self.points):
+            self.partialRecompose(n)
+            n *= 2
