082b9f437ee762c4806ae79f82de1cfc42268100
David Desobry <David.Desobry@ensimag.grenoble-inp.fr>
2019-11-19 15:27:38 +0100

    creation du fichier .nc en loadant a partir de meteo france les potes

---
+++ Projet/Src/data/downloadData.py
@@ -0,0 +1,40 @@
+#!/bin/env python3
+
+import os
+import sys
+import time
+
+if __name__ == "__main__":
+
+# ARGUMENT 1 = NBRE D'HEURE A PARTIR DU MOMENT OU LA COMMANDE EST EXECUTEE
+# ARGUMENT 2 = NOM DU PACKAGE METEOFRANCE A TELECHARGER (SP1, SP2, SP3, HP1)
+
+    if (len(sys.argv) != 5):
+    	print("MAUVAIX NOMBRE D'ARGUMENTS. Format : 0^1 HeureDebut HeureFin Mode")
+    	sys.exit(1)
+    else:
+        (nom, isHD, heureDebut, heureFin, mode) = (sys.argv[0], int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), sys.argv[4])
+        if isHD == 0:
+            scriptArome = "RequeteArome.py"
+            os.system("rm *.grib2")
+            compteur = len([name for name in os.listdir('.') if os.path.isfile(name)])
+            while len([name for name in os.listdir('.') if os.path.isfile(name)]) < compteur + 1:
+                print(compteur)
+                os.system("python2 " + scriptArome + " " + str(heureFin) + " " + mode)
+                time.sleep(3)
+                compteur += 1
+
+        elif isHD == 1:
+            scriptArome = "RequeteAromeHD.py"
+            os.system("rm *.grib2")
+            compteur = len([name for name in os.listdir('.') if os.path.isfile(name)])
+            for i in range(heureDebut, heureFin + 1):
+                print("NEW I : ", i)
+                while len([name for name in os.listdir('.') if os.path.isfile(name)]) < compteur + 1:
+                    print(compteur)
+                    os.system("python2 " + scriptArome + " " + str(i) + " " + mode)
+                    time.sleep(3)
+                compteur += 1
+            os.system("cat $(ls *.grib2) > donneesGroupees.grib2")
+        else:
+            sys.exit("0 ou 1 pour le premier parametre svp")
---
+++ Projet/Src/run.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+
+import os
+import sys
+import time
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 5):
+        print("MAUVAIX NOMBRE D'ARGUMENTS. Format : 0^1 HeureDebut HeureFin Mode")
+        sys.exit(1)
+    else:
+        os.system("cd data;./downloadData.py "+ (" ").join(sys.argv[1:]) + \
+        ";./wgrib2 donneesGroupees.grib2 -netcdf resultat.nc;cd ..")
