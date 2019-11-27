4aea0858622f3bbab763483bb9062776881e5c35
ananas <louteranas@gmail.com>
2019-11-27 23:16:10 +0100

    started changes

--- Projet/Src/data/downloadData.py
+++ Projet/Src/data/downloadData.py
@@ -20,22 +20,32 @@ if __name__ == "__main__":
             compteur = len([name for name in os.listdir('.') if os.path.isfile(name)])
             #while len([name for name in os.listdir('.') if os.path.isfile(name)]) < compteur + 1:
             #    print(compteur)
-            os.system("python2 " + scriptArome + " " + str(heureFin) + " " + mode)
+            print("###########  Telechargement du contenu SP1  ###########")
+            os.system("python2 " + scriptArome + " " + str(heureFin) + " " + "SP1")
             time.sleep(3)
             compteur += 1
-            os.system("cat $(ls *.grib2) > donneesGroupees.grib2")
-
+            os.system("cat $(ls *.grib2) > donneesSP1.grib2")
+            print("###########  Telechargement du contenu IP2  ###########")
+            os.system("python2 " + scriptArome + " " + str(heureFin) + " " + "IP2")
+            time.sleep(3)
+            compteur += 1
+            os.system("cat $(ls *.grib2) > donneesIP2.grib2")
         elif isHD == 1:
-            scriptArome = "RequeteAromeHD.py"
-            os.system("rm *.grib2")
-            compteur = len([name for name in os.listdir('.') if os.path.isfile(name)])
-            for i in range(heureDebut, heureFin + 1):
-                print("NEW I : ", i)
-                while len([name for name in os.listdir('.') if os.path.isfile(name)]) < compteur + 1:
-                    print(compteur)
-                    os.system("python2 " + scriptArome + " " + str(i) + " " + mode)
-                    time.sleep(3)
-                compteur += 1
-            os.system("cat $(ls *.grib2) > donneesGroupees.grib2")
+            ## check if donneesSP1.grib2 AND donneesIP2.grib2 exists and use them
+        elif isHD == 2:
+            ## check if donneesSP1.nc AND donneesIP2.nc exists and use them
+
+        # elif isHD == 1:
+        #     scriptArome = "RequeteAromeHD.py"
+        #     os.system("rm *.grib2")
+        #     compteur = len([name for name in os.listdir('.') if os.path.isfile(name)])
+        #     for i in range(heureDebut, heureFin + 1):
+        #         print("NEW I : ", i)
+        #         while len([name for name in os.listdir('.') if os.path.isfile(name)]) < compteur + 1:
+        #             print(compteur)
+        #             os.system("python2 " + scriptArome + " " + str(i) + " " + mode)
+        #             time.sleep(3)
+        #         compteur += 1
+        #     os.system("cat $(ls *.grib2) > donneesGroupees.grib2")
         else:
             sys.exit("0 ou 1 pour le premier parametre svp")
